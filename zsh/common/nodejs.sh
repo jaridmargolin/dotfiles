fb76374593f7a2085b75ab6443daccb5cfee25ba
Jarid Margolin <jaridmargolin@gmail.com>
2023-01-19 15:28:31 -0800

    Remove unused nodenv config in node.sh

--- zsh/common/nodejs.sh
+++ zsh/common/nodejs.sh
@@ -9,6 +9,3 @@
 # fixes npm global install permissions by moving npm global location
 export NPM_CONFIG_PREFIX=~/.npm-global
 export PATH=~/.npm-global/bin:$PATH
-
-eval "$(nodenv init -)"
-nodenv global 16.13.0

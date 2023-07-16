e1869b64b5f9ef0ce0bb06c693435433297584c0
Jarid Margolin <jaridmargolin@gmail.com>
2023-07-16 13:09:42 -0700

    Fix error caused by using colorls by using exa instead

--- zsh/common/utl.sh
+++ zsh/common/utl.sh
@@ -8,26 +8,24 @@
 #
 # ##############################################################################
 
+alias cp="cp -r"        # Recursive by default
+alias mkdir="mkdir -pv" # Create required and log
 
-alias cp="cp -r"                          # Recursive by default
-alias mkdir="mkdir -pv"                   # Create required and log
-alias ls="ls -G"                          # Of course I want color!
+alias ls="exa --icons --grid --classify --colour=auto --sort=type --group-directories-first --header --modified --created --git --binary --group"
+alias la="ls -a"
+alias ll="ls -al"
 
-alias lc='colorls --sd'
-alias ll="lc -lA --sd"                    # Better list implementation
+alias path='echo -e ${PATH//:/\\n}' # Echo all executable Paths
 
-alias path='echo -e ${PATH//:/\\n}'       # Echo all executable Paths
-
-mcd() { mkdir -p "$1" && cd "$1"; }       # Makes new Dir and jumps inside
-cpwd() { pwd | xargs echo -n | pbcopy; }  # Copy the current working directory
-kpop() { lsof -ti:"$1" | xargs kill; }    # Kill process on specified port
-
+mcd() { mkdir -p "$1" && cd "$1"; }      # Makes new Dir and jumps inside
+cpwd() { pwd | xargs echo -n | pbcopy; } # Copy the current working directory
+kpop() { lsof -ti:"$1" | xargs kill; }   # Kill process on specified port
 
 # While there is no way the api is identical, this suffices from my need to
 # pipe stdout from a command to the macos system clipboard.
 alias xclip='pbcopy'
 
-set-title(){
+set-title() {
   DISABLE_AUTO_TITLE="true"
   WARP_DISABLE_AUTO_TITLE="true"
   echo -ne "\033]0;$@\007"

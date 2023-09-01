758f7a87c3f91fb9d1ea8f81011e5781c763ba05
Jarid Margolin <jaridmargolin@gmail.com>
2023-09-01 14:38:34 -0700

    Add dprune helper fn for docker

--- zsh/common/docker.sh
+++ zsh/common/docker.sh
@@ -11,3 +11,14 @@
 
 dock() { eval $(docker-machine env $1) }
 undock() { eval $(docker-machine env -u) }
+
+# prune docker images
+dprune() {
+    prune_images=$(docker images --filter "dangling=true" -q --no-trunc)
+    if [ -n "$prune_images" ]; then
+        echo "Pruning images"
+        docker rmi $prune_images
+    else
+        echo "No images to prune"
+    fi
+}
